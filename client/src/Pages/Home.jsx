6cba3b1850c40cf7533d236339a96bbba2fffa5c
King Mudassir <mudassaratwork@gmail.com>
2026-03-11 11:45:56 +0500

    c
    c

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,9 @@
-import { Car, DollarSign, MessageSquareText, Scale, User, Menu } from 'lucide-react';
+import React from 'react'
 
 function Home() {
-    return (
-        <div className='mt-20'>
-            <Menu />
-        </div>
-    )
+  return (
+    <div>Home</div>
+  )
 }
 
 export default Home
